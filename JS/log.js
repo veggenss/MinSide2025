df507181b1e81a9197d9cee20adcf3d7e540470b
veggenss <veggens.v@gmail.com>
2025-04-30 18:16:14 +0200

    log js changes

--- JS/log.js
+++ JS/log.js
@@ -1,36 +1,62 @@
 //Checks if there is an input in the search bar
-document.getElementById('search').addEventListener('input', filterLogs);
+const searchInputCheck = document.getElementById('search').value.toLowerCase();
+if (searchInputCheck.length()<1) {
+    console.log("WORKS");
+}
+else {
+    document.getElementById('search').addEventListener('input', filterLogs);
+}
 
 // Add event listener for month filter
-document.getElementById('filterSelect').addEventListener('change', filterByMonth);
+document.getElementById('monthFilterSelect').addEventListener('change', filterByMonth);
+
+//add eventlistener to day filter
+document.getElementById('dayFilterSelect').addEventListener('change', filterByDay);
 
 function filterByMonth() {
-    const selectedMonth = document.getElementById('filterSelect').value.toLowerCase();
+    const selectedMonth = document.getElementById('monthFilterSelect').value.toLowerCase();
     const logs = document.querySelectorAll('.log');
 
     logs.forEach(log => {
-        if (selectedMonth === 'all') {
+        if (selectedMonth === 'all' || log.classList.contains(selectedMonth)) {
             log.style.display = 'block';
         } 
         else {
-            const monthId = log.id.toLowerCase();
-            
-            if (monthId.includes(selectedMonth)) {
+            log.style.display = 'none';
+            }
+    })
+}
+
+
+function filterByDay() {
+    const selectedDay = document.getElementById('dayFilterSelect').value.toLowerCase();
+    const logs = document.querySelectorAll('.log');
+
+    logs.forEach(log => {
+        if (selectedDay === 'all' || log.classList.contains(selectedDay)) {
+            log.style.display = 'block';
+        }
+        else {
+            const dayId = log.id.toLowerCase();
+
+            if (dayId.includes(selectedDay)) {
                 log.style.display = 'block';
-            } else {
+            }
+            else {
                 log.style.display = 'none';
             }
         }
-    });
+    })
+    
 }
 
 function filterLogs() {
     const searchInput = document.getElementById('search').value.toLowerCase();
     const logs = document.querySelectorAll('.log');
-    
+
     //Searches through all the logs and checks if the search input matches the title or content of the log
     logs.forEach(log => {
-        const titleElement = log.querySelector('h3', "h6");
+        const titleElement = log.querySelector("h3", "h6");
         const title = titleElement.textContent.toLowerCase();
 
         // Check if search input matches either title or content
